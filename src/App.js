cb2678a45b91f86ba2e5e8706d7e4b04aaa67941
j-leidy <istaylowkeyz@gmail.com>
2023-01-26 11:34:33 -0500

    adding helm to main

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from 'react';
 import './App.css';
 import ParticlesComponent from './Components/Particles/Particles';
 import MainPage from './Pages/MainPage';
+import { Helmet } from 'react-helmet';
 function App() {
   const [lightordark, setLightOrDark] = React.useState(false)
   return (
     <div className='remove_scroll'>
       <ParticlesComponent active={lightordark} />
       <MainPage setLightOrDark={setLightOrDark} />
+      <Helmet>
+        <title>John Leidy II Portfolio Website</title>
+        <meta name='description' content='This website is a portfolio website created by John Leidy. It is built using React.js' />
+      </Helmet>
     </div>
 
   );
